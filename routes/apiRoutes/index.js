859b6f6e7d2e76ccc0ccdce9221bf6de1940671c
Debbie Neflas <debbieneflas@gmail.com>
2022-05-19 22:56:34 -0700

    separate notes function into lib

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,30 @@
 const router = require('express').Router();
-const [ db ] = require('../../db/db.json');
+const db = require('../../db/db.json');
+const createNewNote = require('../../lib/notes');
 
 
 // api routes
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get('/notes', (req, res) => {
-    res.send('get notes is working');
+    res.json(db);
 });
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, 
 //and then return the new note to the client. You'll need to find a way to give each note a unique id 
 //when it's saved (look into npm packages that could do this for you).
 router.post('/notes', (req, res) => {
-    res.send('post notes is working');
+    const newNote = createNewNote(req.body, db);
+    console.log(newNote);
+    res.json(newNote);
+
+});
+
+// DELETE /api/notes/:id should receive a query parameter containing
+//  the id of a note to delete. In order to delete a note, you'll need to read 
+// all notes from the db.json file, remove the note with the given id property, and 
+// then rewrite the notes to the db.json file.
+
+router.delete('notes/:id', (req, res) => {
+
 });
 
 module.exports = router;
